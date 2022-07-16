ab65faee2959a4594b9e74c6ba0a7643e9143a98
urabe-chihiro <chihiro.js@gmail.com>
2022-07-16 19:20:29 +0900

    feat: fetch todos by apollo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,23 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: `${process.env.NEXT_PUBLIC_BACKEND_URL}/query`,
+  cache: new InMemoryCache(),
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
